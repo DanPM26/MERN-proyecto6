274cd47fa12ce824e4795c2ab47706e1b6f120bc
Dany Ponce <siponce.dwfc5@ucamp.utel.edu.mx>
2022-08-05 14:12:53 -0500

    se modificó el puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./src/app');
 require('dotenv').config()
 require('./src/db/mongodb')
 
-const port = 4000
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`El servidor esta corriendo en el puerto ${port}`)
